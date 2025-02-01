fd6d81fd42bf8458a8df9f386b07214fd1bbc323
Dwarak-10 <dwarakeshvaran0512@gmail.com>
2025-02-01 09:58:03 +0530

    creating components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuComponent } from "./menu/menu.component";
+import { MenuItemComponent } from "./menu/menu-item/menu-item.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ MenuComponent, MenuItemComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
